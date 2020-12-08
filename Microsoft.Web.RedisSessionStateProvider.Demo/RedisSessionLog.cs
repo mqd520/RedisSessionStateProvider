97201929f1ed8cbe2a8fc3e20114f4f0bb5bb7ed
leon <1506816370@qq.com>
2020-12-08 17:42:32 +0800

    11

--- Microsoft.Web.RedisSessionStateProvider.Demo/RedisSessionLog.cs
+++ Microsoft.Web.RedisSessionStateProvider.Demo/RedisSessionLog.cs
@@ -10,7 +10,13 @@ namespace Microsoft.Web.RedisSessionStateProvider.Demo
     {
         public static TextWriter OnWrite()
         {
-            string path = AppDomain.CurrentDomain.BaseDirectory + "Log.txt";
+            string dir = AppDomain.CurrentDomain.BaseDirectory + "Logs\\RedisSession\\";
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            string path = string.Format("{0}RedisSessionLog-{1}.txt", dir, DateTime.Now.ToString("yyyy-MM-dd_HH-mm-dd"));
             FileStream fs = File.Open(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
 
             return new StreamWriter(fs);
